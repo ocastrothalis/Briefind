b74f857fe6ec5f4f98396fe3691938d1f2ce1bf4
ocastrothalis <thalissonfjv@gmail.com>
2023-04-28 10:05:18 -0300

    Atualização 28/04/23

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-const cursor = document.querySelector('.cursor');
-let link = document.querySelectorAll('.link')
-let levelUp = document.querySelectorAll('.levelup')
-let loader = document.querySelector('.loader')
-
-window.addEventListener('load', vanish)
-
-function vanish(){
-    loader.classList.add('disppear')
-}
-
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute('style', 'top: ' + (e.pageY - 10) + 'px; left: ' + (e.pageX - 10) + 'px;')
-})
-
 let menuToggle = document.querySelector(".menuToggle")
 let navigation = document.querySelector(".navigation")
 
